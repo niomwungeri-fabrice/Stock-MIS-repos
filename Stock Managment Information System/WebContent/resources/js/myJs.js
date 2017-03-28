c73b95402540ccad3251fd85a057d851b542780e
NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2017-03-28 19:57:44 +0200

    Initial Commit

---
+++ Stock Managment Information System/WebContent/resources/js/myJs.js
@@ -0,0 +1,3 @@
+function changeColor(){
+	alert("Color Changed");
+}
---
+++ Stock Managment Information System/src/rw/itcg/test/TestMyApp.java
@@ -0,0 +1,17 @@
+package rw.itcg.test;
+
+/**
+ * @author NIYOMWUNGERI
+ * Mar 28, 2017, 7:48:34 PM
+ */
+public class TestMyApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
