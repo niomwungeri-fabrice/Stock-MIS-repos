bbb369d41eb50fff8c550aae49cff1847dca4d3f
NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2017-03-28 20:08:34 +0200

    new configurations added

--- Stock Managment Information System/src/rw/itcg/test/TestMyApp.java
+++ Stock Managment Information System/src/rw/itcg/test/TestMyApp.java
@@ -1,8 +1,7 @@
 package rw.itcg.test;
 
 /**
- * @author NIYOMWUNGERI
- * Mar 28, 2017, 7:48:34 PM
+ * @author NIYOMWUNGERI Mar 28, 2017, 7:48:34 PM
  */
 public class TestMyApp {
 
@@ -11,7 +10,6 @@ public class TestMyApp {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello");
-
 	}
 
 }
