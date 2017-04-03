a517efdb1b9e3ac91a746763802970884ac40870
NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2017-04-03 23:30:19 +0200

    some changes

--- Stock Managment Information System/src/rw/itcg/usecase/GenerateQuotes.java
+++ Stock Managment Information System/src/rw/itcg/usecase/GenerateQuotes.java
@@ -3,17 +3,19 @@ package rw.itcg.usecase;
 import java.util.Random;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 /**
  * @author NIYOMWUNGERI Apr 3, 2017, 9:40:57 PM
  */
 @ManagedBean
+@SessionScoped
 public class GenerateQuotes {
 
-	public String generateRandomQuotes() {
+	public String getQaute() {
 		String[] array = {
-				"“Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do”.” - Pele",
-				"“Success means doing the best we can with what we have. Success is the doing, not the getting; in the trying, not the triumph. Success is a personal standard, reaching for the highest that is in us, becoming all that we can be.”<br /> – Zig Ziglar" };
+				"Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do - Pele",
+				"Success means doing the best we can with what we have. Success is the doing, not the getting; in the trying, not the triumph. Success is a personal standard, reaching for the highest that is in us, becoming all that we can be. – Zig Ziglar" };
 		Random rand = new Random();
 		int lenngth = rand.nextInt(array.length);
 		return array[lenngth];
