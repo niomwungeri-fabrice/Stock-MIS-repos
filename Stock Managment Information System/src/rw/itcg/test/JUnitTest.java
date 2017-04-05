bf68163112839419e620212f4bfac82e85995e49
NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2017-04-05 09:39:11 +0200

    expression resolved

--- Stock Managment Information System/src/rw/itcg/test/JUnitTest.java
+++ Stock Managment Information System/src/rw/itcg/test/JUnitTest.java
@@ -1,11 +1,14 @@
 package rw.itcg.test;
 
+import rw.itcg.usecase.GenerateQuotes;
+
 /**
  * @author NIYOMWUNGERI Mar 28, 2017, 9:15:45 PM
  */
 public class JUnitTest {
 
 	public static void main(String args[]) {
-
+			GenerateQuotes gQuotes = new GenerateQuotes();
+			System.out.println(gQuotes.getQaute());
 	}
 }
--- Stock Managment Information System/src/rw/itcg/usecase/GenerateQuotes.java
+++ Stock Managment Information System/src/rw/itcg/usecase/GenerateQuotes.java
@@ -5,11 +5,6 @@ import java.util.Random;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import rw.itcg.dao.QuotesDao;
-import rw.itcg.service.QuoteService;
-
 /**
  * @author NIYOMWUNGERI Apr 3, 2017, 9:40:57 PM
  */
@@ -17,10 +12,26 @@ import rw.itcg.service.QuoteService;
 @SessionScoped
 public class GenerateQuotes {
 
-	public String getQaute() {
+	private String quotes;
+
+	public String getQuotes() {
+		return quotes;
+	}
+
+	public void setQuotes(String quotes) {
+		this.quotes = quotes;
+	}
+
+	public GenerateQuotes() {
+		this.quotes = GenerateQuotes.getQaute();
+	}
+
+	public static String getQaute() {
 		String[] array = {
 				"Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do - Pele",
-				"Success means doing the best we can with what we have. Success is the doing, not the getting; in the trying, not the triumph. Success is a personal standard, reaching for the highest that is in us, becoming all that we can be. – Zig Ziglar" };
+				"Success means doing the best we can with what we have. Success is the doing, not the getting; in the trying, not the triumph. Success is a personal standard, reaching for the highest that is in us, becoming all that we can be. – Zig Ziglar",
+				"To me, job titles don’t matter. Everyone is in sales. It’s the only way we stay in business. – Harvey Mackay",
+				"Remember that failure is an event, not a person. Yesterday ended last night. – Zig Ziglar"};
 		Random rand = new Random();
 		int lenngth = rand.nextInt(array.length);
 		return array[lenngth];
